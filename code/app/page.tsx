9a9c767702bfa70776bd76c0df7e7eb69057fa91
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-06-04 03:50:29 +0530

    made page.tsx

--- code/app/page.tsx
+++ code/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="flex flex-col align-center justify-center h-screen gap-4 max-w-md mx-auto">
         <a href="/login">go to login page</a>
       </div>
     </>
